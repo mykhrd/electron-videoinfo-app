0e0e28eaf7c0941c697dc7a903fc629ef97451a9
Miyuki Harada <mh@glue-inc.net>
2017-12-07 11:54:05 +0900

    edit index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+let mainWindow;
 
 //event handler to watch
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('videoData', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        // console.log('Video duration is:', metadata.format.duration);
+        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
 });
